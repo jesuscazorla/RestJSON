f8a2a5d8f35b15bbd5f5566b2679d2e20d8aabc5
mac <mac>
2022-12-11 11:39:26 +0100

    Initial Commit

---
+++ RestJSON/RestJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestJSONApp.swift
+//  RestJSON
+//
+//  Created by Aula11 on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RestJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
